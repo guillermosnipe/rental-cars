9a44945b1e609f5a7767b053d60dd732c4fabb0e
guillermosnipe <snipeglk@gmail.com>
2021-05-29 14:28:46 +0200

    fix: Car Search title and subtitle issue fixed

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <Header title="Welcome to the app!" cssClasses="columns is-mobile is-centered px-4 mb-0" />
-      <CarSearch cssClasses="columns is mobile is-centered is-multiline p-5"/>
+      <CarSearch cssClasses="columns is mobile is-centered is-multiline p-5" title="Car Hire – Search, Compare &amp; Save"/>
       <Footer cssClasses="has-text-centered" />
     </>
   );
--- src/components/CarSearch.jsx
+++ src/components/CarSearch.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const CarSearch = ({ title, cssClasses, placeholder }) => {
+const CarSearch = ({ title, subtitle, cssClasses, placeholder }) => {
   return (
     <section className={cssClasses ? 'car-search ' + cssClasses : 'car-search'}>
       <h2 className="has-text-centered column is-full">{title}</h2>
       <div className="car-search__search-box column is-two-thirds-desktop">
-        <h3>{title || "Let’s find your ideal car"}</h3>
+        <h3>{subtitle || "Let’s find your ideal car"}</h3>
         <div className="mt-2">
           <input type="text" placeholder={placeholder || "Pick-up Location"} className="mr-2"/>
           <button className="primary">Search</button>
