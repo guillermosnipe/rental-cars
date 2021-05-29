a922f523fcbb382596fd2facba2e3c20e03256e3
Guillermo Snipe <guillermo@backbase.com>
2021-05-29 14:24:09 +0200

    feat: basic layout and styling

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './theme/App.scss';
 // components
 import Header from './components/Header';
 import Footer from './components/Footer';
+import CarSearch from './components/CarSearch';
 
 function App() {
   return (
-    <div className="App">
-      <Header title="Welcome to the app!" />
-      <Footer />
-    </div>
+    <>
+      <Header title="Welcome to the app!" cssClasses="columns is-mobile is-centered px-4 mb-0" />
+      <CarSearch cssClasses="columns is mobile is-centered is-multiline p-5"/>
+      <Footer cssClasses="has-text-centered" />
+    </>
   );
 }
 
