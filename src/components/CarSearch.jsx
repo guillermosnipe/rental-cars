a22655569f7457397ecc936ba22df3e66864fe7d
guillermosnipe <snipeglk@gmail.com>
2021-05-30 22:43:34 +0200

    refactor: car search logic moved to search term

--- src/components/CarSearch.jsx
+++ src/components/CarSearch.jsx
@@ -1,50 +1,9 @@
-import React, { useRef, useEffect, useState } from "react";
-import AwesomeDebouncePromise from 'awesome-debounce-promise';
-import callSearchAPI from "../lib/api";
+import React from "react";
 
 // components
 import SearchTerm from "./SearchTerm";
 
 const CarSearch = ({ title, subtitle, cssClasses, placeholder }) => {
-  const [searchTermValue, setSearchTermValue] = useState("");
-  const [returnedSearchResults, setReturnedSearchResults] = useState([]);
-
-  const handleSearchTermChange = (event) => {
-    setSearchTermValue(event.target.value);
-  };
-
-  const fetchSearchResults = async (searchTerm) => {
-    const searchAPI = AwesomeDebouncePromise(callSearchAPI, 500);
-
-    if (searchTerm.length > 1) {
-      
-      const searchResults = await searchAPI(searchTerm);
-
-      if (searchResults.data.results.numFound > 0) {
-        setReturnedSearchResults(searchResults.data.results.docs);
-      } else {
-        // If no search results were matched, the api returns the "empty" object.
-        setReturnedSearchResults(searchResults.data.results.docs);
-      }
-
-    } else {
-      setReturnedSearchResults([]);
-    }
-  };
-
-  // Storing a ref. This is going to tell us if this is the initial render.
-  const firstUpdate = useRef(true);
-
-  // Hook to manage the search term length and trigger the call if applies
-  useEffect(() => {
-    // TODO: Understand how this works. Check the ref
-    if (firstUpdate.current) {
-      firstUpdate.current = false;
-      return;
-    }
-
-    fetchSearchResults(searchTermValue);
-  }, [searchTermValue]);
 
   return (
     <section className={cssClasses ? "car-search " + cssClasses : "car-search"}>
@@ -56,11 +15,7 @@ const CarSearch = ({ title, subtitle, cssClasses, placeholder }) => {
             data-role="search-results-input"
             className="column is-9-desktop is-full-tablet is-full-mobile is-relative"
           >
-            <SearchTerm
-              handleSearchFn={handleSearchTermChange}
-              searchResults={returnedSearchResults}
-              placeholder={placeholder}
-            />
+            <SearchTerm placeholder={placeholder} />
           </div>
           <div
             data-role="search-results-button"
