c07eba3576f2b1e84ca311dd75a193af5c2ecb9f
guillermosnipe <snipeglk@gmail.com>
2021-06-01 21:27:10 +0200

    refactor: getting rid of code that was not needed at all. debouncing it's working now

--- src/components/SearchTerm.jsx
+++ src/components/SearchTerm.jsx
@@ -1,45 +1,29 @@
 import React, { useRef, useEffect, useState } from "react";
-import AwesomeDebouncePromise from 'awesome-debounce-promise';
+import AwesomeDebouncePromise from "awesome-debounce-promise";
 import callSearchAPI from "../lib/api";
 
 // components
-import SearchResultsDropdown from './SearchResultsDropdown';
+import SearchResultsDropdown from "./SearchResultsDropdown";
+const searchAPI = AwesomeDebouncePromise(callSearchAPI, 350);
 
-const SearchTerm = ({
-  placeholder
-}) => {
-
+const SearchTerm = ({ placeholder }) => {
   const [searchTermValue, setSearchTermValue] = useState("");
   const [returnedSearchResults, setReturnedSearchResults] = useState([]);
   const [isHidden, setIsHidden] = useState(false);
-  const searchAPI = AwesomeDebouncePromise(callSearchAPI, 750);
 
   const handleSearchTermChange = (event) => {
-    setSearchTermValue(event.target.value);
+      setSearchTermValue(event.target.value);
   };
 
   const handleOnFocus = () => {
     if (returnedSearchResults.length > 0 && isHidden) {
       setIsHidden(false);
     }
-  }
+  };
 
   const fetchSearchResults = async (searchTerm) => {
-
-    if (searchTerm.length > 1) {
-      
-      const searchResults = await searchAPI(searchTerm);
-
-      if (searchResults.data.results.numFound > 0) {
-        setReturnedSearchResults(searchResults.data.results.docs);
-      } else {
-        // If no search results were matched, the api returns the "empty" object.
-        setReturnedSearchResults(searchResults.data.results.docs);
-      }
-
-    } else {
-      setReturnedSearchResults([]);
-    }
+    const searchResults = await searchAPI(searchTerm);
+    setReturnedSearchResults(searchResults.data.results.docs);
   };
 
   // Storing a ref. This is going to tell us if this is the initial render.
@@ -47,42 +31,51 @@ const SearchTerm = ({
 
   // Hook to manage the search term length and trigger the call if applies
   useEffect(() => {
+
+    console.log(typeof searchTermValue);
+
     if (firstUpdate.current) {
       firstUpdate.current = false;
       return;
     }
-    fetchSearchResults(searchTermValue);
+
+    if (searchTermValue.length > 1) {
+      fetchSearchResults(searchTermValue);
+    } else {
+      setReturnedSearchResults([]);
+    }
 
   }, [searchTermValue]);
 
   const wrapperRef = useRef(null);
   useOutsideAlerter(wrapperRef);
 
-
   function useOutsideAlerter(ref) {
     useEffect(() => {
-        /**
-         * Alert if clicked on outside of element
-         */
-        function handleClickOutside(event) {
-            if (ref.current && !ref.current.contains(event.target)) {
-                setIsHidden(true);
-            }
+      /**
+       * Alert if clicked on outside of element
+       */
+      function handleClickOutside(event) {
+        if (ref.current && !ref.current.contains(event.target)) {
+          setIsHidden(true);
         }
+      }
 
-        // Bind the event listener
-        document.addEventListener("mousedown", handleClickOutside);
-        return () => {
-            // Unbind the event listener on clean up
-            document.removeEventListener("mousedown", handleClickOutside);
-        };
+      // Bind the event listener
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        // Unbind the event listener on clean up
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
     }, [ref]);
-}
+  }
 
-  return(
+  return (
     <div ref={wrapperRef}>
-      <label htmlFor="searchTermInput" className="is-sr-only">Enter the pick-up location</label>
-      <input 
+      <label htmlFor="searchTermInput" className="is-sr-only">
+        Enter the pick-up location
+      </label>
+      <input
         type="text"
         placeholder={placeholder || "Pick-up Location"}
         className="mr-2"
@@ -91,9 +84,12 @@ const SearchTerm = ({
         autoComplete="off"
         onFocus={handleOnFocus}
       />
-      <SearchResultsDropdown results={returnedSearchResults} cssClasses={isHidden ? "is-hidden" : ""} />
+      <SearchResultsDropdown
+        results={returnedSearchResults}
+        cssClasses={isHidden ? "is-hidden" : ""}
+      />
     </div>
-  )
-}
+  );
+};
 
 export default SearchTerm;
